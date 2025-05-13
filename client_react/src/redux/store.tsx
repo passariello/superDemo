c36a533878c62b26073a1bfcb184c3b32735bbaf
Dario Passariello <dariopassariello@gmail.com>
2025-05-12 23:11:56 -0600

    NPM update

--- client_react/src/redux/store.tsx
+++ client_react/src/redux/store.tsx
@@ -3,11 +3,11 @@ Copyright: © 2022 Dario Passariello <dariopassariello@gmail.com>
 License: CC BY-NC-ND 4.0
 */
 
-import thunk from "redux-thunk"
-import rootReducer from "Root/redux/reducers/combine";
+import { thunk } from 'redux-thunk'
+import rootReducer from "Root/redux/reducers/combine"
 //import httpMiddleware from "./middleware/http"
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { createBrowserHistory } from "history"
 import { routerMiddleware } from "connected-react-router"
@@ -16,10 +16,10 @@ export const history = createBrowserHistory()
 export const configureStore = () => {
 
   const store = createStore(
-    rootReducer( history ),
+    rootReducer(history),
     composeWithDevTools(
       applyMiddleware(
-        routerMiddleware( history ),
+        routerMiddleware(history),
         thunk,
         //httpMiddleware
       )
